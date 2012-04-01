56e73d4f342bd1e69fc1a9e560822868122ab664
Taras Kushnir <taras.kushnir@eleks.com>
2012-04-01 20:17:53 +0300

    Started pawn promotion dialog development

---
+++ ChessProgram/ChessBoardLib/ViewModel/PawnPromotionItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Queem.Core;
+using ChessBoardVisualLib.Enums;
+
+namespace ChessBoardVisualLib.ViewModel
+{
+    public class PawnPromotionItem : DependencyObject
+    {
+        public PawnPromotionItem(Figure figure, Color color)
+        {
+            this.UpdateChessFigure(figure, color);
+        }
+
+        public static readonly DependencyProperty FigureTypeProperty =
+                   DependencyProperty.Register("FigureType", typeof(Figure), typeof(PawnPromotionItem));
+
+        public Figure FigureType
+        {
+            get { return (Figure)GetValue(FigureTypeProperty); }
+            set { SetValue(FigureTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty FigureColorProperty =
+                    DependencyProperty.Register("FigureColor", typeof(Color), typeof(PawnPromotionItem));
+
+        public Color FigureColor
+        {
+            get { return (Color)GetValue(FigureColorProperty); }
+            set { SetValue(FigureColorProperty, value); }
+        }
+
+        public void UpdateChessFigure(Figure newFigure, Color newColor)
+        {
+            this.FigureType = newFigure;
+            this.FigureColor = newColor;
+            this.ColoredFigure = ColoredFigureHelper.Create(this.FigureColor, this.FigureType);
+        }
+
+        public static readonly DependencyProperty ColoredFigureProperty =
+            DependencyProperty.Register("ColoredFigure", typeof(ColoredFigure), typeof(PawnPromotionItem));
+
+        public ColoredFigure ColoredFigure
+        {
+            get { return (ColoredFigure)GetValue(ColoredFigureProperty); }
+            set { SetValue(ColoredFigureProperty, value); }
+        }
+    }
+}
---
+++ ChessProgram/ChessBoardLib/ViewModel/PawnPromotionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Queem.Core;
+using System.Collections.ObjectModel;
+
+namespace ChessBoardVisualLib.ViewModel
+{
+    public class PawnPromotionViewModel : ObservableObject
+    {
+        private Figure[] promotionFigures = 
+            new Figure[] { Figure.Knight, Figure.Bishop, Figure.Rook, Figure.Queen };
+
+        private ObservableCollection<PawnPromotionItem> promotionItems;
+
+        public PawnPromotionViewModel()
+        {
+
+        }
+
+        public ObservableCollection<PawnPromotionItem> Items
+        {
+            get { return this.promotionItems; }
+        }
+    }
+}
